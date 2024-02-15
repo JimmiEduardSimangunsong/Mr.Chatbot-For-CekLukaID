0a3f17985f9447bd680fe49ff089a2f7151ab60f
Jimmi Eduard Simangunsong <jimmi24eduard@gmail.com>
2024-02-15 15:33:51 +0700

    Make a Good Code

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ CORS(app)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-vertexai.init(project="capstone-project-c23-pc635", location="us-central1")
+vertexai.init(project="ceklukaid", location="us-central1")
 chat_model = ChatModel.from_pretrained("chat-bison")
 parameters = {
     "candidate_count": 1,
     "max_output_tokens": 1024,
-    "temperature": 0.2,
-    "top_p": 0.8,
-    "top_k": 40
-}
+    "temperature": 0.9,
+    "top_p": 1
+},
+
 chat = chat_model.start_chat(
     context="""Anda adalah Mr.Chat Luka, chatbot layanan pengguna untuk Cek Luka App Anda hanya menjawab pertanyaan 
     pelanggan tentang dunia kesehatan, terutama Pennaganan Luka Ringan dan luka, pola hidup sehat, saran kesehatan,hindari membahas diluar konteks kesehatan.""",
@@ -27,10 +27,10 @@ chat = chat_model.start_chat(
 # Define a Socket.IO event handler for receiving messages
 @socketio.on('message')
 def handle_message(msg):
-    response = chat.send_message(msg, **parameters)
+    response = chat.send_message(msg)
     # Send the response back to the client that sent the original message
     send(str(response.text))
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=8080, use_reloader=True)
+    socketio.run(app,host='0.0.0.0', port=5000, use_reloader=True)
